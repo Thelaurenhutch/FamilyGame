4d285a2933e13ab3c98a5b94417e2c416e58ec2e
thelaurenhutch <thelaurenhutch@gmail.com>
2024-09-23 14:13:48 -0400

    modified:   js/mainGame.js 	deleted:    smaller_images/another_day.png 	deleted:    smaller_images/apartment.png 	new file:   smaller_images/ascream.PNG 	deleted:    smaller_images/at_apartment.png 	deleted:    smaller_images/at_beach.png 	deleted:    smaller_images/backhome.png 	deleted:    smaller_images/base_evergetable.png 	deleted:    smaller_images/beach_disaster.png 	new file:   smaller_images/black.PNG 	deleted:    smaller_images/boat.png 	deleted:    smaller_images/bookstore_activities.png 	deleted:    smaller_images/breaking_window.png 	deleted:    smaller_images/cave.png 	deleted:    smaller_images/cheese_castle.png 	deleted:    smaller_images/cheeseland.png 	deleted:    smaller_images/corn_space.png 	deleted:    smaller_images/dandelion.png 	new file:   smaller_images/darknoise.PNG 	new file:   smaller_images/drinks.PNG 	deleted:    smaller_images/evergetable_climb.png 	deleted:    smaller_images/favicon.jpg 	deleted:    smaller_images/forgetmenot.png 	new file:   smaller_images/ginnydoor.PNG 	new file:   smaller_images/ginnypoint.PNG 	deleted:    smaller_images/glowing_popcorn.png 	deleted:    smaller_images/glowing_space_orb.png 	deleted:    smaller_images/grocery_bag.png 	deleted:    smaller_images/grocery_bag_out.png 	deleted:    smaller_images/happy_king.png 	deleted:    smaller_images/having_beach_fun.png 	deleted:    smaller_images/hiding.png 	deleted:    smaller_images/in_fridge.png 	deleted:    smaller_images/in_pantry.png 	new file:   smaller_images/kevinphone.PNG 	deleted:    smaller_images/mad_gouda.png 	deleted:    smaller_images/mushroom.png 	deleted:    smaller_images/nightfalls.png 	deleted:    smaller_images/no_response.png 	deleted:    smaller_images/out_fridge.png 	deleted:    smaller_images/performance.png 	new file:   smaller_images/pickleader.PNG 	deleted:    smaller_images/placeholder.png 	deleted:    smaller_images/pull_lever.png 	deleted:    smaller_images/room.png 	new file:   smaller_images/running.PNG 	new file:   smaller_images/smell.PNG 	deleted:    smaller_images/snackies.png 	deleted:    smaller_images/space_confusion.png 	deleted:    smaller_images/space_travel.png 	deleted:    smaller_images/spaceplan.png 	deleted:    smaller_images/tomato.png 	deleted:    smaller_images/vegetal_favicon.png 	deleted:    smaller_images/vortex.png 	deleted:    smaller_images/where_to.png 	deleted:    smaller_images/which_book.png 	deleted:    smaller_images/window_jump.png 	deleted:    smaller_images/window_locked.png 	new file:   smaller_images/youfell.PNG 	new file:   smaller_images/zachbush.PNG 	new file:   smaller_images/zap.PNG 	new file:   smaller_images/zoomgoop.PNG

--- js/mainGame.js
+++ js/mainGame.js
@@ -2,7 +2,7 @@ const gameData = {
     "1": {
         "text": "You and your family are enjoying some cocktails in the speakeasy. \
             What are you sipping on?",
-        "image" : "smaller_images/snackies.png",
+        "image" : "smaller_images/drinks.PNG",
 "choices": {
             "A whiskey drink": [2,["Lauren", "Zach", "Kevin"]],
             "A vodka drink": [2,["Faith", "Holland",]],
@@ -14,7 +14,7 @@ const gameData = {
         "text": " Suddenly a smell permeates the air. \
             It's disgusting. Horrible. \
             How do you react?",
-        "image" : "smaller_images/where_to.png",
+        "image" : "smaller_images/smell.PNG",
 "choices": {
             "Scream": [3, ["Patti", "Zach", "One of the Kids"]],
             "Go investigate": [4,["Madison", "Holland", "Kevin"]],
@@ -24,7 +24,7 @@ const gameData = {
    "3":{
         "text": "As you scream you notice the rest of the family rush into the other room.\
                 That's weird? Should you follow?",
-        "image" : "smaller_images/mad_gouda.png",
+        "image" : "smaller_images/running.PNG",
 "choices" : {
             "Well, I'm not staying in the phantom smell room alone.": [6,["Lauren", "Logan", "Patti", "Zach"]],
             "Yeah, I just want to be involved.":[6, ["Madison", "One of the Kids"]],
@@ -35,7 +35,7 @@ const gameData = {
         "text": "Before you get up to investigate, a loud screech comes from the adjoining room.\
                 \"WHO DID THIS! EVERYONE COME LOOK!\" \
                 What do you do?",
-        "image" : "smaller_images/mad_gouda.png",
+        "image" : "smaller_images/ascream.PNG",
 "choices" : {
             "Run in there. I have to see.": [6,["Patti", "Kevin", "Holland", "Madison"]],
             "Yell back \"STOP YELLING!\"":[6, ["Lauren", "Logan", "Faith"]],
@@ -46,7 +46,7 @@ const gameData = {
         "text": "While you were minding your business, a loud screech comes from the adjoining room.\
                 \"WHO DID THIS! EVERYONE COME LOOK!\" \
                 What do you do?",
-        "image" : "smaller_images/mad_gouda.png",
+        "image" : "smaller_images/ascream.PNG",
 "choices" : {
             "Run in there. I have to see.": [6,["Patti", "Kevin", "Holland", "Madison"]],
             "Yell back \"STOP YELLING!\"":[6, ["Lauren", "Logan", "Faith"]],
@@ -58,7 +58,7 @@ const gameData = {
                 Upon closer inspection it's a child that belongs, here!--Ginny? \
                 The stench has grown significantly stronger... \
                 In front of her, there is a ...a pile?... of what??",
-        "image" : "smaller_images/mad_gouda.png",
+        "image" : "smaller_images/ginnydoor.PNG",
 "choices" : {
             "Welp, I'm out.": [7,["Faith", "Lauren", "Holland", "Logan"]],
             "I immediately assume Ginny did this":[7, ["Patti", "One of the Kids", "Madison"]],
@@ -69,7 +69,7 @@ const gameData = {
         "text": "As you turn to walk away you slip\
                 You are face to face with this mystery goop\
                 What's that? You notice...",
-        "image" : "smaller_images/mad_gouda.png",
+        "image" : "smaller_images/youfell.PNG",
 "choices" : {
             "These floors are filthy! They really need to sweep more": [8,["Holland", "Lauren"]],
             "There is a shiny glint in the pile, you investigate further":[8, ["Patti", "One of the Kids", "Madison"]],
@@ -80,7 +80,7 @@ const gameData = {
         "text": "You pull out your magnifying glass that you always have on hand\
                 You tell the family we must vote for a non-partisan leader to solve this mystery \
                 They agree, who does the family choose?",
-        "image" : "smaller_images/mad_gouda.png",
+        "image" : "smaller_images/zoomgoop.PNG",
 "choices" : {
             "Kevin- i mean he already has the costume": [9,["Holland", "Lauren"]],
             "Logan- he's the only one that can truly be non-partisan":[50, ["Patti", "One of the Kids", "Madison"]],
@@ -91,7 +91,7 @@ const gameData = {
         "text": "Kevin immediately runs to put on his detective suit and gets into character.\
                 He looks closely at the glint in the pile \"Yes, yes... a clue\"\
                 He pulls out the object and shows everyone. it's a phone. and... and IT'S DEAD!",
-        "image" : "smaller_images/mad_gouda.png",
+        "image" : "smaller_images/kevinphone.PNG",
 "choices" : {
             "Continue": [10,[]],
         }
@@ -102,7 +102,7 @@ const gameData = {
                 Wait Ginny is still here. She seems traumatized. She points into the kitchen \
                 \"Well, Well, Well\" Kevin continues. He looks at you for advice, \
                 \"Which of these should we investigate?\"",
-        "image" : "smaller_images/mad_gouda.png",
+        "image" : "smaller_images/ginnypoint.PNG",
 "choices" : {
             "The fur. Seems like the simplest, and I have a drink to finish": [11,["Patti", "Logan", "Madison", "Zach"]],
             "The phone. Clearly, someone is hiding something":[12, ["Holland", "Faith"]],
@@ -114,7 +114,7 @@ const gameData = {
                 Nobody has them on them. Everyone is a suspect. \
                 The house is a mess, it will be hard to find them all \
                 What should we do",
-        "image" : "smaller_images/mad_gouda.png",
+        "image" : "smaller_images/kevinphone.PNG",
 "choices" : {
             "Let's just plug this one in and see what happens": [14,["Logan", "Holland", "Kevin", "Zach"]],
             "We need to delve into the chaos and all separate to find our phones":[20, ["Lauren", "Faith"]],
@@ -125,7 +125,7 @@ const gameData = {
         "text": "You plug in the phone. \
                 it SPARKS! \
                 The power goes out in the house.",
-        "image" : "smaller_images/mad_gouda.png",
+        "image" : "smaller_images/zap.PNG",
 "choices" : {
             "Starting looking around for a source of light": [15,["Zach"]],
             "Cry":[16, ["Holland", "Faith"]],
@@ -136,7 +136,7 @@ const gameData = {
         "text": "You walk around looking for a source of light\
                 You know there is a flashlight in here somewhere\
                 Just as you grab the light you hear a weird sound coming from outside",
-        "image" : "smaller_images/mad_gouda.png",
+        "image" : "smaller_images/darknoise.PNG",
 "choices" : {
             "Go outside": [18,["Zach"]],
             "Go back to the family":[19, ["Holland", "Faith"]]
@@ -146,7 +146,7 @@ const gameData = {
         "text": "You sit down to give up home and ask God to end it all when you \
                 feel someone snatch the phone from your hand \
                 You still can't see. What do you do?",
-        "image" : "smaller_images/mad_gouda.png",
+        "image" : "smaller_images/black.PNG",
 "choices" : {
             "Go outside": [18,["Zach"]],
             "Go back to the family":[19, ["Holland", "Faith"]]
@@ -156,7 +156,7 @@ const gameData = {
         "text": "You start to scream when you feel \
                 someone snatch the phone from your hand \
                 You still can't see. What do you do?",
-        "image" : "smaller_images/mad_gouda.png",
+        "image" : "smaller_images/black.PNG",
 "choices" : {
             "Go outside to get some air": [18,["Zach"]],
             "Scream louder": [20, ["Faith"]],
@@ -166,7 +166,7 @@ const gameData = {
    "18":{
         "text": "A small amount of light peeks into the bush where the weird noise is coming from \
                 it's Zach! What's he doing?", 
-        "image" : "smaller_images/mad_gouda.png",
+        "image" : "smaller_images/zachbush.PNG",
 "choices" : {
             "He's throwing up in the bush again- it was him!": [69420,["Zach"]],
             "I'm gonna mind my business and go back inside":[19, ["Holland", "Faith"]]
@@ -176,7 +176,7 @@ const gameData = {
         "text": "You start screaming louder \
         Suddenly in your ear you hear Faith. \
         \"Goddamn WHY\" ",
-        "image" :"smaller_images/mad_gouda.png",
+        "image" :"smaller_images/drinks.PNG",
 "choices" : {
             "Why is she so close to me?--ITS HER PHONE!": [21, [Faith]],
             "For dramatic effect." :[19, [Faith], [Holland]],
@@ -186,7 +186,7 @@ const gameData = {
     "21":{
         "text": "You start to swat around at Faith \
         You hear the unquestionable sound of a phone hitting the floor ", 
-        "image" : "smaller_images/mad_gouda.png",
+        "image" : "smaller_images/drinks.PNG",
 "choices" : {
             "start feeling around on the ground to get it back": [22,["Faith"]],
             "tackle faith!!!!": [22,["Faith"]]
@@ -195,7 +195,7 @@ const gameData = {
     "22":{
         "text": "You hit the ground and immediatley notice a different smell \
         Tequila. It's kind of... familiar... more than just a bad friday night. ", 
-        "image" : "smaller_images/mad_gouda.png",
+        "image" : "smaller_images/drinks.PNG",
 "choices" : {
             "Hold Faith down and smell her": [23,["Faith"]],
             "Go find your way back into the room with the goop": [24,["Faith"]]
@@ -204,7 +204,7 @@ const gameData = {
     "23":{
         "text": "Faith does not consent to this. \
         But you got a good whiff. It's like the goop! ", 
-        "image" : "smaller_images/mad_gouda.png",
+        "image" : "smaller_images/drinks.PNG",
 "choices" : {
             "I don't know exactly but faith might have something to do with the goop. I'll just accuse her!": [25,["Faith"]],
             "NOW Go find your way back into the room with the goop": [24,["Faith"]]
@@ -213,7 +213,7 @@ const gameData = {
     "25":{
         "text": "Do you want to figure out why?\
         Or just accuse her and be done with it? ", 
-        "image" : "smaller_images/mad_gouda.png",
+        "image" : "smaller_images/drinks.PNG",
 "choices" : {
             "Ill investigate. Go back to the goop!": [24,["Faith"]],
             "IT'S ALWAYS FAITH": [69420,["Faith"]]
@@ -222,7 +222,7 @@ const gameData = {
     "24":{
         "text": "You get back to the goop, just as Kevin turns the lights back on.\
         What do you want to do? ", 
-        "image" : "smaller_images/mad_gouda.png",
+        "image" : "smaller_images/drinks.PNG",
 "choices" : {
             "Look inside the goop to see any more clues": [25,["Faith"]],
             "START TEARING THINGS APART!!!": [26,["Faith"]]
@@ -231,7 +231,7 @@ const gameData = {
     "25":{
         "text": "You look closely into the goop you start to notice.. is that a lime peel?\
         ... and ice? Was this a *gasp* margarita? ", 
-        "image" : "smaller_images/mad_gouda.png",
+        "image" : "smaller_images/drinks.PNG",
 "choices" : {
             "FAITH DROPPED HER MARGARITA! IT WAS HER!": [69420,["Faith"]],
             "okay it might be a marg but like wtf is the fur": [11,["Faith"]]
@@ -241,7 +241,7 @@ const gameData = {
         "text": "You knock over a drink and a goop like substance pours on the floor\
         you compare it to the goop.. is that a lime peel?\
         ... and ice? Was this a *gasp* margarita? ", 
-        "image" : "smaller_images/mad_gouda.png",
+        "image" : "smaller_images/drinks.PNG",
 "choices" : {
             "FAITH DROPPED HER MARGARITA! IT WAS HER!": [69420,["Faith"]],
             "okay it might be a marg but like wtf is the fur": [11,["Faith"]]
@@ -249,28 +249,28 @@ const gameData = {
     }, 
    "19":{
         "text": "You go back to the family. ", 
-        "image" : "smaller_images/mad_gouda.png",
+        "image" : "smaller_images/drinks.PNG",
 "choices" : {
             "ahhhhh": [69420,["Zach"]]
         }
     },
    "11":{
         "text": "test test test test", 
-        "image" : "smaller_images/mad_gouda.png",
+        "image" : "smaller_images/drinks.PNG",
 "choices" : {
             "ahhhhh": [69420,["Zach"]]
         }
     },
    "50":{
         "text": "test test test test", 
-        "image" : "smaller_images/mad_gouda.png",
+        "image" : "smaller_images/drinks.PNG",
 "choices" : {
             "ahhhhh": [69420,["Zach"]]
         }
     },
    "30":{
         "text": "test test test test", 
-        "image" : "smaller_images/mad_gouda.png",
+        "image" : "smaller_images/drinks.PNG",
 "choices" : {
             "ahhhhh": [69420,["Zach"]]
         }
@@ -288,7 +288,7 @@ const gameData = {
         "text": "You yell out to the family you have solved the mystery. \
                 But based on the choices you made did you do enough? \
                 Are you absolutely sure?",
-        "image" : "smaller_images/another_day.png",
+        "image" : "smaller_images/drinks.PNG",
 "choices" : {
             "Yes, I know who did it": [0,[]],
             "No but im over this": [0,[]],
